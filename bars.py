fa51b2c4e61a05b70038cc9eb6d23aecc258f381
Z4PH0D <dreamfall3r@gmail.com>
2018-04-01 14:46:57 +0300

    Возможно так на вложенность ругаться не будет..

--- bars.py
+++ bars.py
@@ -8,9 +8,9 @@ def ask_file_path():
 def ask_user_location():
     try:
         return [float(x) for x in input
-                ('Введите долготу и широту(только'
-                 ' цифры и разделительные точки): ')
-                .split()]
+        ('Введите долготу и широту(только'
+         ' цифры и разделительные точки): ')
+            .split()]
     except (ValueError, TypeError):
         print('Неправильно введены данные')
 
@@ -25,29 +25,29 @@ def load_json(filepath):
 
 def get_biggest_bar(bars_list):
     return max(bars_list, key=lambda bar: bar['properties']
-                                             ['Attributes']
-                                             ['SeatsCount'])
+    ['Attributes']
+    ['SeatsCount'])
 
 
 def get_smallest_bar(bars_list):
     return min(bars_list, key=lambda bar: bar['properties']
-                                             ['Attributes']
-                                             ['SeatsCount'])
+    ['Attributes']
+    ['SeatsCount'])
 
 
 def get_closest_bar(bars_list, longitude, latitude):
-    return min(bars_list, key=lambda bar:
-               abs(bar['geometry']['coordinates'][0] - longitude) -
-                   abs(bar['geometry']['coordinates'][1] - latitude))
-
+    closest_bar = min(bars_list, key=lambda bar:
+    abs((bar['geometry']['coordinates'][0] - longitude) -
+        (bar['geometry']['coordinates'][1] - latitude)))
+    return closest_bar
 
 def print_choices():
     return int(input('''Что Вы хотите найти?
     1) Самый большой бар
     2) Самый маленький бар
-    3) Ближайший бар
-    0) Выйти из программы\n
+    3) Ближайший бар\n
     '''))
+
 
 def get_bars_list():
     return load_json(ask_file_path())['features']
@@ -56,7 +56,7 @@ def get_bars_list():
 if __name__ == '__main__':
     user_choice = print_choices()
     bars_list = get_bars_list()
-    
+
     if user_choice == 1:
         print('Самый большой бар: ',
               str(get_biggest_bar(bars_list))
@@ -69,4 +69,6 @@ if __name__ == '__main__':
 
     elif user_choice == 3:
         user_longitude, user_latitude = ask_user_location()
-        get_closest_bar(bars_list, user_longitude, user_latitude)
+        print('Ближайший бар: ',
+        str(get_closest_bar(bars_list, user_longitude, user_latitude))
+              .replace('{','').replace('}',''))
