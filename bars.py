2906d5d06336958021519726c0083869b4679545
Z4PH0D <dreamfall3r@gmail.com>
2018-04-04 01:09:28 +0300

    Добавил сообщения в exit, поправил форматирование, добавил проверку на количество аргументов

--- bars.py
+++ bars.py
@@ -17,8 +17,7 @@ def load_data(filepath):
 def get_biggest_bar(bars_list):
     bar = max(
         bars_list,
-        key=lambda bar:
-        bar['properties']['Attributes']['SeatsCount']
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
     )
     return bar
 
@@ -26,8 +25,7 @@ def get_biggest_bar(bars_list):
 def get_smallest_bar(bars_list):
     bar = min(
         bars_list,
-        key=lambda bar:
-        bar['properties']['Attributes']['SeatsCount']
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
     )
     return bar
 
@@ -49,7 +47,10 @@ def get_bars_list(loaded_data):
 
 if __name__ == '__main__':
 
-    filepath = sys.argv[1]
+    if len(sys.argv) == 2:
+        filepath = sys.argv[1]
+    else:
+        sys.exit('Вы не указали путь к файлу!')
 
     try:
         bars_list = get_bars_list(load_data(filepath))
@@ -60,16 +61,17 @@ if __name__ == '__main__':
     try:
         user_latitude, user_longitude = get_user_location()
     except(ValueError, TypeError):
-        exit()
+        sys.exit('Вы ввели неверные координаты!')
 
-    print('Самый большой бар: ', str(
-        get_biggest_bar(bars_list)['properties']['Attributes']['Name'])
-          )
-    print('Самый маленький бар: ', str(
-        get_smallest_bar(bars_list)['properties']['Attributes']['Name'])
-          )
+    print('Самый большой бар: ',
+          get_biggest_bar(bars_list)['properties']['Attributes']['Name'])
+
+    print('Самый маленький бар: ',
+          get_smallest_bar(bars_list)['properties']['Attributes']['Name'])
+
     print('Ближайший бар: ',
-          str(get_closest_bar(
-                  bars_list, user_latitude, user_longitude
-              )['properties']['Attributes']['Name'])
-          )
+          get_closest_bar(
+              bars_list,
+              user_latitude,
+              user_longitude
+          )['properties']['Attributes']['Name'])
